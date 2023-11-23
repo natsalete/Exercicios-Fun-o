31462758d637082eecf62f8f3fc12f5a67dc19e8
natalete <natsalete14@gmail.com>
2023-11-23 20:28:03 -0300

    Desafio 3

---
+++ desafio3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <math.h>
+
+using namespace std;
+
+ void numero(int n){
+    for(int i = 1; i <= n; i++){
+        cout << i << endl;
+    }
+    return ;
+}
+
+
+int main(){
+
+    int n;
+
+    cin >> n;
+
+    numero(n);
+
+
+
+    return 0;
+}
+
+
