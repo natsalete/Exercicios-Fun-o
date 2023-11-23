21e0c4d5fec47a42da6478784f317e6d7e89976c
natalete <natsalete14@gmail.com>
2023-11-23 19:49:31 -0300

    Desafio 1

---
+++ desafio1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include <math.h>
+
+using namespace std;
+
+// 1. Crie uma função capaz de receber 3 números a, b e c, e retornar o delta desses números.
+
+double maior(double x, double y){
+    int maior = 0;
+     if(x > y){
+       maior  
+     }
+    return delta;
+}
+
+
+int main(){
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    delta(a, b, c);
+
+    cout << delta << endl;
+
+
+    return 0;
+}
+
